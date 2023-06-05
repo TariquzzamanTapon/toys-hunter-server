ce41d05d7b3ffb38c849ca1a46421aefd911887c
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-05 21:06:28 +0600

    Database are created

--- index.js
+++ index.js
@@ -25,11 +25,21 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollection = client.db('toysDB').collection('toys');
+
+       
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
