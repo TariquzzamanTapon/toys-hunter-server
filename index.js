770f19fa7d73f419157f21c04c7135bc2f5d01c7
TarifHasan <tariquzzaman.tapon@gmail.com>
2023-06-05 21:07:04 +0600

    Toys are creted

--- index.js
+++ index.js
@@ -32,9 +32,13 @@ async function run() {
 
         const toysCollection = client.db('toysDB').collection('toys');
 
-       
+        app.get('/toys', async(req, res)=>{
+            const cursor = toysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);     
+        })
 
-
+        
 
 
 
